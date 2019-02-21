b35be766327115c720cb7e28a1d9a37f1f713569
bboye003 <bboye003@plattsburgh.edu>
2019-02-21 00:21:28 -0500

    update to gui

--- gui_prac.py
+++ gui_prac.py
@@ -1,6 +1,13 @@
 #!/usr/local/bin/python3
 
-import tkinter
-top = tkinter.Tk()
+#import tkinter
+#top = tkinter.Tk()
 
-top.mainloop()
+#top.mainloop()
+from tkinter import *
+
+window = Tk()
+
+window.title("Welcome to this screen")
+
+window.mainloop()
